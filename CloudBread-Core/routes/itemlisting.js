9be4e69dbab072d0c60a1142ecc4c1a07cb94f35
Edwin222 <sleepyrainyday@naver.com>
2017-11-12 00:38:48 +0900

    Return Item 구현
    * Return Item 기능을 즉시 환불로 이해하고 구현(유효한지 확인 필요)
    
    * Return Item과 Item Purchase의 Request Method를 post로 변경.
    
    * router 밖에서 함수로 따로 구현한 것들을 파일 상단으로 빼서 모아둠.

--- CloudBread-Core/routes/itemlisting.js
+++ CloudBread-Core/routes/itemlisting.js
@@ -3,10 +3,47 @@ let models = require('../models');
 
 let router = express.Router();
 
+
+let getPurchaseLog = (purchase_id) => {
+    let result_purchase;
+    models.MemberItemPurchase.find({
+        where: { MemberItemPurchaseID: String(purchase_id) }
+    }).then(result => {
+        result_purchase = result;
+    });
+
+    return result;
+};
+
+let getInventoryItem = (member_id, item_id) => {
+    let result_item;
+    models.MemberItems.find({
+        where: {
+            MemberID: String(member_id),
+            ItemListID: String(item_id)
+        }
+    }).then(result => {
+        result_item = result;
+    });
+
+    return result;
+};
+
+let selectItem = function (id) {
+    let target_id = String(id);
+
+    models.ItemList.findAll({
+        where: { ItemListID: target_id }
+    }).then((result) => {
+        return result;
+    });
+};
+
+
 /* Router for Item Listing system */
 
 /****************************************/
-/* selectItemList                       */
+/* B23: selectItemList                       */
 /* param List                           */
 /* int::page = Number of Page to lookup */
 /* String::memberid = taget member's id */
@@ -24,39 +61,30 @@ router.get("/selectItemList", (req, res, next) => {
 });
 
 /****************************************/
-/* selectItem                           */
+/* B26: selectItem                           */
 /* param List                           */
 /* String::itemid = taget item's id     */
 /****************************************/
-let selectItem = function (id) {
-    let target_id = String(id);
-
-    models.ItemList.findAll({
-        where: { ItemListID: target_id }
-    }).then((result) => {
-        return result;
-    });
-};
-
 router.get("/selectItem", (req, res, next) => { 
     console.log("in selectitem");
     res.json(selectItem(req.query.itemid));
 });
 
-
 /****************************************/
-/* addMemberItemPurchase                */
+/* B27: ItemPurchase                */
 /* param List                           */
 /* String::itemid = target item's id    */
 /* String::memberid = target member's id */
-/* String::region = target item */
+/* String::region = target record's region  */
 /* int::price = item price */
 /* String::quantity = item quantity */
 /* String::deviceID = buyer's device id */
 /* String::IP = buy's ip address */
 /* String::MAC = buy's MAC address */
 /****************************************/
-router.get("/addMemberItemPurchase", (req, res, next) => {
+router.post("/ItemPurchase", (req, res, next) => {
+
+    //get parameter from query
     let target_item_id = String(req.query.itemid);
     let target_member_id = String(req.query.memberid);
     let target_region = String(req.query.region);
@@ -139,9 +167,74 @@ router.get("/addMemberItemPurchase", (req, res, next) => {
         });
 
     }
+    res.end();
+});
 
+/****************************************/
+/* B30: returnItem                */
+/* param List                           */
+/* String::itemid = target item's id    */
+/* String::memberid = target member's id */
+/* String::purchaseid = returned purchase log's id */
+/* String::deviceID = target's device id */
+/* String::IP = target's IP address */
+/* String::MAC = target's MAC adddress */
+/****************************************/
+router.post("/returnItem", (req, res, next) => {
+    /**
+     * Need Validation for implementation
+     */
+    let target_item_id = String(req.query.itemid);
+    let target_member_id = String(req.query.memberid);
+    let target_purchase_id = String(req.query.purchaseid);
+    let target_device_id = String(req.query.deviceID);
+    let target_ip = String(res.query.IP);
+    let target_MAC = String(res.query.MAC);
 
-    res.json(result);
+    //select returned purchase
+    let returned_purchase = getPurchaseLog(target_purchase_id);
+
+    //get item from inventory
+    let returned_item = getInventoryItem(target_member_id, target_item_id);
+
+    //modify purchase record's status to returned
+    models.MemberItemPurchase.update({
+        PurchaseCancelYN: String('Y'),
+        PurchaseCancelingStatus: String(3),
+        PurchaseCancelReturnedAmount: returned_purchase.PurchasePrice,
+        PurchaseCancelDeviceID: target_device_id,
+        PurchaseCancelDeviceIPAddress: target_ip,
+        PurchaseCancelDeviceMACAddress: target_MAC,
+        where: { MemberItemPurchaseID: target_purchase_id }
+    }).then(result => {
+        returned_purchase = result;
+    });
+
+    //modify item's count 
+    models.MemberItems.update({
+        ItemCount: String(parseInt(returned_item.ItemCount) - parseInt(returned_purchase.PurchaseQuantity)),
+        where: {
+            MemberID: target_member_id,
+            ItemListID: target_item_id
+        }
+    }).then(result => {
+        returned_item = result;
+    });
+
+    //delete item from inventory when count <= 0
+    if (returned_item.ItemCount <= 0) {
+        models.MemberItems.destroy({
+            where: {
+                MemberID: target_member_id,
+                ItemListID: target_item_id
+            }
+        }).then(result => {
+            returned_item = {};
+        });
+    }
+
+    res.end();
+
 });
 
 /* Implement message router here */
