857f2958293903d11f636eea8f11e64a66b4cedc
young <siosio34@nate.com>
2017-11-19 20:31:26 +0900

    Add express middlewar lib

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const azureMobileApps = require('azure-mobile-apps');
 
+const expressValidator = require('express-validator');
+const expressStatusMonitor = require('express-status-monitor');
+const errorHandler = require('errorhandler');
+
 // require router
 // const administrator = require('./routes/administrator');
 // const game = require('./routes/game');
@@ -20,10 +24,6 @@ const app = express();
 // hompage : true => instance of Azure-Mobile-Apps for hompage
 const mobile = azureMobileApps({ homePage: true });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -31,6 +31,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressValidator());
+app.use(expressStatusMonitor());
+app.use(errorHandler());
 
 // Setup Router
 // app.use('/admin', administrator);
@@ -47,23 +50,5 @@ app.use('/api', api);
 mobile.tables.add('index');
 // Add the mobile API so it is accessible as a Web API
 app.use(mobile);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.send(`error${err.status}`);
-});
 
 module.exports = app;
