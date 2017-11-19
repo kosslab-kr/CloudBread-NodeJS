99d3581d61e7a179a70c7ffb8a558c99064007dc
young <siosio34@nate.com>
2017-11-19 17:40:47 +0900

    Apply tab size in airbnb lint

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,9 @@
 const express = require('express');
+
 const router = express.Router();
 
 const controllers = require('../controllers');
-const cbPing = require('../controllers/CBPingController')
+const cbPing = require('../controllers/CBPingController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,9 +15,6 @@ router.get('/ping', controllers.CBPingController.get);
 
 /* To-do : 각각 컨트롤러로 구현 */
 router.post('/CBAddMemberItemPurchase', controllers.CBAddMemberItemPurchaseController.Post);
-
 router.post('/CBAddUseMemberItem', controllers.CBAddUseMemberItemController.Post);
-
-
 
 module.exports = router;
